c98cb84cf50c5af2261764042490a0cd58951905
Pig Nachos <piaignaciara@gmail.com>
2020-08-18 21:46:04 -0400

    comenzar proyecto formulario javascript

--- validar.js
+++ validar.js
@@ -2,8 +2,11 @@ function validar(){
     const nombre = document.getElementById("nombre").value;
     const apellidos = document.getElementById("apellidos").value;
     const correo = document.getElementById("correo").value;
+    const usuario = document.getElementById("usuario").value;
+    const telefono = document.getElementById("telefono").value;
+    const contraseña = document.getElementById("pass").value;
 
-    if (nombre === "" || apellidos === "") {
+    if (nombre === "" || apellidos === "" || correo === "" || usuario === "" || telefono === "" || contraseña === "") {
         alert("Todos los campos son obligatorios");
         return false;
     }
@@ -19,9 +22,26 @@ function validar(){
         alert("el correo debe poseer un ‘@’ y un punto ‘.’");
         return false;
     }
+    if (correo.length>100) {
+        alert("El correo es muy largo");
+        return false;
+    }
+    if (usuario.length>20) {
+        alert("El nombre de usuario es muy largo");
+        return false;
+    }
+    if (telefono.length>15) {
+        alert("El telefono es muy largo");
+        return false;
+    }
+    if (!telefono.match(/^\d+$/g)) {
+        alert("el telefono solo debe llevar numeros");
+        return false;
+    }
 
 
-    alert(saludo);
+    alert(`nombre: ${nombre}, usuario: ${usuario}, password: ${contraseña}`)
+
 }
 
 
